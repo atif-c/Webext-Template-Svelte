4096fe1d42cb81bb1029f10bad0a553f6b740180
atif-c <atif-c>
2025-08-22 15:50:42 +0100

    Formatting

--- src/lib/storage/utils.ts
+++ src/lib/storage/utils.ts
@@ -42,7 +42,7 @@ export function cleanObject<T extends object>(
             !Array.isArray(templateItem)
         ) {
             // Recursively clean each array item if it's an object
-            return userArr.map((item) =>
+            return userArr.map(item =>
                 typeof item === 'object' &&
                 item !== null &&
                 !Array.isArray(item)
@@ -52,7 +52,7 @@ export function cleanObject<T extends object>(
         } else {
             // Primitive array items - accept user array if all items are same type as template item
             const typeOfTemplateItem = typeof templateItem;
-            if (userArr.every((item) => typeof item === typeOfTemplateItem)) {
+            if (userArr.every(item => typeof item === typeOfTemplateItem)) {
                 return userArr;
             }
             return templateArr;
